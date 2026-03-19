314572d9f65237e4362d6840dc76a4d725a0b8c4
dlawo1133-bit <dlawo1133@gmail.com>
2026-03-19 18:27:11 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,8 +5,8 @@ self.addEventListener("install", (event) => {
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll([
         "/",
-        "/index.html",
-        "/manifest.json",
+        "./index.html"
+        "./manifest.json"
         "/icon-192.png",
         "/icon-512.png"
       ]);
